2375eaa5dd46ddedb40c1a87960b14b8a2df5004
Andrew Kolb <akolb87@gmail.com>
2019-05-29 22:15:18 -0500

    storing guesses

--- src/components/user-guess.js
+++ src/components/user-guess.js
@@ -2,14 +2,52 @@ import React from 'react';
 import GuessedNumbers from './guessed-numbers';
 // import './user-guess.css';
 
-export default function UserGuess(props) {
-  return (
-    <div>   
-        <form>
-        <input type="number" id="userGuess" min="1" max="100" required/>
-        <button type="submit" id="guessButton">Guess</button>
-      </form>
-      <GuessedNumbers />
-    </div>
-  );
+export default class UserGuess extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numbersGuessed: []
+        }
+    }
+    submitNumber = (event) => {
+        event.preventDefault();
+        console.log(this.input.value)
+        let currentNumbers = this.state.numbersGuessed;
+        currentNumbers.push(this.input.value)
+        
+        this.setState({numbersGuessed: currentNumbers})
+        console.log(this.state.numbersGuessed)
+
+        // if (this.props.onMakeGuess) {
+        //     const value = this.input.value;
+        //     this.props.onMakeGuess(value);
+        // }
+        // this.input.value = '';
+        // this.input.focus();
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.submitNumber}>
+                    <input
+                        type="number"
+                        id="userGuess"
+                        min="1"
+                        max="100"
+                        ref={input => (this.input = input)}
+                        required
+                    />
+                    <button
+                        type="submit"
+                        id="guessButton"
+                        className="button"
+                    >
+                        Guess
+          </button>
+                </form>
+                <GuessedNumbers />
+            </div>
+        );
+    }
 }
